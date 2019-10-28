77148458aeed410a3dc4ec22d71f3f57546871c2
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-10-28 15:41:08 +1100

    add dashboard headline

--- app.js
+++ app.js
@@ -1,20 +1,91 @@
-// Traffic Graph
-var ctx = document.getElementById('traffic-graph').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+// // Traffic Graph
+// const trafficGraph = document.getElementById('traffic-graph').getContext('2d');
+const chart = new Chart(trafficGraph, {
+   type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+   data: {
+      labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+      
+      datasets: [{
+         label:'Hourly',
+         data: [0, 500, 1000, 725, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+         backgroundColor: 'rgba(116,119,191,0.4)',
+         borderColor: 'rgba(116,119,191,1)',
+         borderWidth:2,
+         lineTension:0,
+         pointRadius:6,
+         pointBorderColor:'rgba(116,119,191,1)',
+         pointBackgroundColor:'white',
+         pointBorderWidth:'2',
+         fontColor:'red',
+         fontSize:'12',
+         fontFamily:'arial',
+      },{
+         label:'Daily',
+         data: [0, 500, 1000, 725, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+         backgroundColor: 'rgba(116,119,191,0.4)',
+         borderColor: 'rgba(116,119,191,1)',
+         borderWidth:2,
+         lineTension:0,
+         pointRadius:6,
+         pointBorderColor:'rgba(116,119,191,1)',
+         pointBackgroundColor:'white',
+         pointBorderWidth:'2',
+         fontColor:'red',
+         fontSize:'12',
+         fontFamily:'arial',
+       
+      },
+      {
+         label:'Weekly',
+         data: [0, 500, 1000, 725, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+         backgroundColor: 'rgba(116,119,191,0.4)',
+         borderColor: 'rgba(116,119,191,1)',
+         borderWidth:2,
+         lineTension:0,
+         pointRadius:6,
+         pointBorderColor:'rgba(116,119,191,1)',
+         pointBackgroundColor:'white',
+         pointBorderWidth:'2',
+         fontColor:'red',
+         fontSize:'12',
+         fontFamily:'arial',
+       
+      },
+      {
+         label:'Monthly',
+         data: [0, 500, 1000, 725, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+         backgroundColor: 'rgba(116,119,191,0.4)',
+         borderColor: 'rgba(116,119,191,1)',
+         borderWidth:2,
+         lineTension:0,
+         pointRadius:6,
+         pointBorderColor:'rgba(116,119,191,1)',
+         pointBackgroundColor:'white',
+         pointBorderWidth:'2',
+         fontColor:'red',
+         fontSize:'12',
+         fontFamily:'arial',
+       
+      }],
+   },
 
-    // Configuration options go here
-    options: {}
+   // Configuration options go here
+   options: {
+      layout: {
+         padding: {
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+         }
+      },
+      legend: {
+         labels: {
+             fontColor: 'red',
+         }
+     }
+
+
+   }
 });
