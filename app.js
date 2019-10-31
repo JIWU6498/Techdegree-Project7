dfe312ff91158bfcd4847cc5e96e3f36b31aba2a
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-10-31 12:14:58 +1100

    finish setting part

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ let lineChartOptions = {
    scales: {
       yAxes: [{
          ticks: {
-            beginAtZero: true
+            beginAtZero: false,
+         },
+         stacked: false,
+         gridLines: {
+            display: true,
+            lineWidth: 1,
          }
       }]
    },
@@ -115,13 +120,11 @@ let pieChartData = {
 
 let pieChartOptions = {
    legend: {
-      display:true,
       position: 'right',
-      align:'center',
       labels: {
          boxWidth: 20,
          fontStyle: 'bold',
-      }
+      },
    },
    layout: {
       padding: {
@@ -132,10 +135,30 @@ let pieChartOptions = {
       }
    },
 
+
 }
 
 let drawPieChart = new Chart(pieChart, {
    type: 'doughnut',
    data: pieChartData,
    options: pieChartOptions,
+});
+
+// Alert Banner
+const alert = document.getElementById('alert');
+
+//create the html for the banner
+alert.innerHTML =
+   `
+   <div class="alert-banner">
+   <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+   to complete</p>
+   <p class="alert-banner-close">x</p>
+   </div>
+   `
+alert.addEventListener('click', e => {
+   const element = e.target;
+   if (element.classList.contains("alert-banner-close")) {
+      alert.style.display = "none";
+   }
 });
